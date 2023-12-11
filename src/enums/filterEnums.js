8887aab0b4664e3084bd2a5b143270750303c23f
Yatharth Surana <yashsurana2010@gmail.com>
2023-12-11 18:27:53 +0530

    Update filterEnums.js

--- src/enums/filterEnums.js
+++ src/enums/filterEnums.js
@@ -2,5 +2,5 @@ export const filterEnums = {
     newProducts: "New Uploads",
     trending: "Most Viewed",
     mostDownload: "Most Downloaded",
-    FromtopUsers: "From top users"
+    FromtopUsers: "From Top Users"
 }
